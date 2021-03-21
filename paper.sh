fd9e0502d7a850d832d5236b0106cc78d0be0cc4
Josh Wright <joshnotwright@gmail.com>
2021-03-21 18:33:36 -0400

    change version

--- paper.sh
+++ paper.sh
@@ -304,7 +304,7 @@ function AnnounceDowntimeUpdate {
 }
 
 # Menu
-choice=$(whiptail --title "TheWrightServer Management Tool v3.7.2" --fb --menu "Select an option" 18 100 10 \
+choice=$(whiptail --title "TheWrightServer Management Tool v3.8" --fb --menu "Select an option" 18 100 10 \
     "1." "Update" \
     "2." "Start" \
     "3." "Stop" \
