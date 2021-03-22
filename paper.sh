7fd28b3ef3646c619adab73abbea4195b2c99a36
Josh Wright <joshnotwright@gmail.com>
2021-03-22 01:20:37 -0400

    fix version number

--- paper.sh
+++ paper.sh
@@ -330,7 +330,7 @@ function BackupRemoveOldest {
 
 
 # Menu
-choice=$(whiptail --title "TheWrightServer Management Tool v3.8" --fb --menu "Select an option" 18 100 10 \
+choice=$(whiptail --title "TheWrightServer Management Tool v3.9 Alpha" --fb --menu "Select an option" 18 100 10 \
     "1." "Update" \
     "2." "Start" \
     "3." "Stop" \
