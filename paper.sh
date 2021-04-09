a7e26d922fbf4d504bb31f5858b86fb27c421ad4
Josh Wright <joshnotwright@gmail.com>
2021-04-09 19:51:03 -0400

    readd 1 was, but not 2

--- paper.sh
+++ paper.sh
@@ -464,7 +464,7 @@ function GetServerStatus {
         if [ $LastUsed -gt 300 ]; then
             echo "$FriendlyName was last used $LastUsedDifference ago and last backed up $LastBackupDifference ago"
         else
-            echo "$FriendlyName is currently being used and last backed up $LastBackupDifference ago"
+            echo "$FriendlyName is currently being used and was last backed up $LastBackupDifference ago"
         fi
     fi
 }
